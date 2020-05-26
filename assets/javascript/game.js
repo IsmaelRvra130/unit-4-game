32362abbbc0f7b26fa69dd87d557343ff6db50af
Ismael Rivera <i.rvra130@gmail.com>
2020-05-25 22:49:26 -0400

    Cleaned up js/jquery

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,123 +1,121 @@
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script type="text/javascript">
-
-    $(document).ready(function() { 
-    //Generates random number between 1-120.
-        var targetNumber = Math.floor(Math.random() * 119) + 1;
-        var numberOptions = Math.floor(Math.random() * 11) + 1;
-        var lockGame = false;
-        $("#number-to-guess").text(targetNumber);
+$(document).ready(function() { 
     
-        var wins = 0; // variables.
-        var losses = 0;
-        var counter = 0;
-        var numberOptions = [];
-    //var winDiv = document.getElementById ("#wins");
-	//var loseDiv = document.getElementById ("#losses");
-
-
-    
-        //Loop for the 4 crystals and their value.
-    for (var i = 0; i < 4; i++) {
-        addCrystalNum();
-        var imageCrystal = $("<img>");
-        imageCrystal.addClass("crystal-image");
-        imageCrystal.attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqjkn0OaLDc9w9qlVH5FRNgevIXPC71hJFIAJ-OOY5b_hDtn3u");
-        imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-        $(".crystal-img").append(imageCrystal);
-
-
-        /*if (i=0) {
-            <img width="125px" height="125px" src= "">
-        }
-        else if (i=1) {
-            <img width="125px" height="125px" src= "">
-        }
-        else if (i=2) {
-            <img width="125px" height="125px" src= "">
-        }
-        else if (i=3) {
-            <img width="125px" height="125px" src= "">
-        }
-        else {
-            alert("image error");
-        }
-        */
-        console.log(numberOptions);
-        }
-        //Randomly generates the value of each crystal after each gameplay.
-        function addCrystalNum(){
-            var newNum = Math.floor(Math.random() * 11) + 1;
-        if (numberOptions.includes(newNum)){
-            var notUsedNum = newNum + 1;
-            numberOptions.push(notUsedNum);
-        }
-        else {
-            numberOptions.push(newNum);
-        }
+    //Generates random numbers
+    var getRandom = function(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
     }
-        //Function for the clicks of the crystals.
-    $(".crystal-image").on("click", function() {
-        var crystalValue = ($(this).attr("data-crystalvalue"));
-        crystalValue = parseInt(crystalValue);
-        counter += crystalValue;
-        alert("New score: " + counter);
-
-
-        if (counter === targetNumber) {
-         alert("You win!");
-         //resetGame()
-         wins ++;
-         //updateWinScore();
-         resetGame();
+    
+    //Generates random number to guess.
+    targetNumber = getRandom(2, 119);
+    
+    //Variables
+    var wins = 0;
+    var losses = 0;
+    var counter = 0;
+    var targetNumber = 0;
+    
+    //Crystal object.
+    var crystal = {
+        blue:
+        {
+            name:"blue",
+            value: 0
+        },
+        green:
+        {
+            name:"green",
+            value:0
+        },
+        red:
+        {
+            name:"red",
+            value:0
+        },
+        yellow:
+        {
+            name:"yellow",
+            value:0
         }
-         else if (counter > targetNumber) {
-        alert("You lose!!");
-        //resetGame()
-        losses ++;
-        //updateLossScore();
-        resetGame();
-        }
+    };
 
+    //Function to start the game.
+    function startGame() {
+        targetNumber = getRandom(1, 119);
+        crystal.blue.value = getRandom(1, 12);
+        crystal.red.value = getRandom(1, 12);
+        crystal.green.value = getRandom(1, 12);
+        crystal.yellow.value = getRandom(1, 12);
+        $("#number-to-guess").text(targetNumber);
+        $("#your-score").text(counter);
         updateWinScore();
         updateLossScore();
+    }   
+    
+    //Calling my functions.
+    startGame();
+    updateWinScore();
+    updateLossScore();
 
+    //update win score function.
+    function updateWinScore() {
+    document.querySelector("#wins").innerHTML = "wins : " + wins;
+    }
 
-        function updateWinScore() {
-        document.querySelector("#wins").innerHTML = "wins : " + wins;
-        //resetGame();
-        }
+    //update loss function.
+    function updateLossScore() {
+    document.querySelector("#losses").innerHTML = "losses :" + losses;
+    }
 
-        function updateLossScore() {
-        document.querySelector("#losses").innerHTML = "losses :" + losses;
-        }
+    //Function that adds value to the crystals.
+    var addvalue = function(clickedCrystal){
+        counter += clickedCrystal.value;
+        $("#your-score").text(counter);
+        checkWin();
+    }
 
-        function resetGame() {
-        location.reset();
-        updateWinScore();
-        updateLossScore();
-        var targetNumber = Math.floor(Math.random() * 119) + 1;
-        var numberOptions = Math.floor(Math.random() * 11) + 1;
-        //location.reload();
-        //targetNumber = String.fromCharCode(crystalValue);    
-	    }
+    //Fuction to reset the game after a play.
+    function resetGame(){
+        counter = 0;
+        startGame()
+         
+    }
 
-
-
-        //Conditions for win or loose.
-        //if (counter === targetNumber) {
-         //alert("You win!");
-         //wins ++;
-         //updateScore();
-         //resetGame();
-        //}
-        //else if (counter >= targetNumber) {
-        //alert("You lose!!");
-        //losses ++;
-        //updateScore();
-        //resetGame();
-       // }   
+    //  MAIN PROCESS
+    // ======================================================
+    
+    //JQUERY ONclick for crystals
+    $("#blue").click(function(){ 
+        addvalue(crystal.blue);  
     });
-});
 
-    </script>  
+    $("#green").click(function(){ 
+        addvalue(crystal.green);  
+    });
+
+    $("#red").click(function(){ 
+        addvalue(crystal.red);  
+    });
+
+    $("#yellow").click(function(){ 
+        addvalue(crystal.yellow);  
+    });
+    
+    //Function for the Win/loss logic.
+    var checkWin = function(){
+        if (counter > targetNumber) {
+            $("#alert").text("Sorry you lost");
+            setTimeout(function() { $("#alert").hide();}, 2000);
+            losses++;
+            updateLossScore()
+            resetGame();    
+        }
+
+        else if ( counter === targetNumber) {
+            $("#alert").text("Congrats!"); 
+            setTimeout(function() { $("#alert").hide();}, 2000);
+            wins++;
+            updateWinScore()
+            resetGame();    
+        }
+    };
+});
